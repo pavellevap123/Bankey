6431bb284fd251cced1e752434b2e61a3125c6c9
Pavel Poddubotskiy <pavel.poddubotskiy@pm.by>
2022-07-26 18:30:12 +0300

    Create generic onboarding view controller2

--- Bankey/Onboarding/OnboardingViewController.swift
+++ Bankey/Onboarding/OnboardingViewController.swift
@@ -9,19 +9,34 @@ import UIKit
 
 class OnboardingViewController: UIViewController {
     
-    let stackView = UIStackView()
-    let imageView = UIImageView()
-    let label = UILabel()
+    private let stackView = UIStackView()
+    private let imageView = UIImageView()
+    private let label = UILabel()
+    
+    private var heroImageName = ""
+    private var titleText = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
         style()
         layout()
     }
+    
+    init(heroImageName: String, titleText: String) {
+        self.heroImageName = heroImageName
+        self.titleText = titleText
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
 }
 
 extension OnboardingViewController {
     private func style() {
+        view.backgroundColor = .systemBackground
+        
         stackView.translatesAutoresizingMaskIntoConstraints = false
         stackView.axis = .vertical
         stackView.spacing = 20
@@ -29,7 +44,7 @@ extension OnboardingViewController {
         // Image
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.contentMode = .scaleAspectFit
-        imageView.image = UIImage(named: "Lion1")
+        imageView.image = UIImage(named: heroImageName)
         
         // Label
         label.translatesAutoresizingMaskIntoConstraints = false
@@ -37,7 +52,7 @@ extension OnboardingViewController {
         label.font = UIFont.preferredFont(forTextStyle: .title3)
         label.adjustsFontForContentSizeCategory = true
         label.numberOfLines = 0
-        label.text = "Bankey is faster, easier to use, and has a brand new look and feel that will make you feel like you are back in 1989."
+        label.text = titleText
     }
     
     private func layout() {
